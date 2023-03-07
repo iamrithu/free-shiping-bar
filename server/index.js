ff25d6b6e1f4747f8e4c6966a84ced6e4443261d
iamrithu <ritimahesh29@gmail.com>
2023-03-07 23:06:10 +0530

    new commit

--- server/index.js
+++ server/index.js
@@ -22,9 +22,9 @@ const isTest = process.env.NODE_ENV === "test" || !!process.env.VITE_TEST_BUILD;
 Shopify.Context.initialize({
   API_KEY: process.env.SHOPIFY_API_KEY,
   API_SECRET_KEY: process.env.SHOPIFY_API_SECRET,
-  // SCOPES: process.env.SCOPES.split(","),
+  SCOPES: process.env.SCOPES.split(","),
   HOST_NAME: process.env.HOST.replace(/https:\/\//, ""),
-  // API_VERSION: ApiVersion.April22,
+  API_VERSION: ApiVersion.April22,
   IS_EMBEDDED_APP: true,
   // This should be replaced with your preferred storage strategy
   SESSION_STORAGE: new Shopify.Session.MemorySessionStorage(),
