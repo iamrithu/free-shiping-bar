cd99b1fe9b3511a8e5b93f465103a3483f0df767
iamrithu <ritimahesh29@gmail.com>
2023-03-07 17:55:41 +0530

    new commit

--- server/index.js
+++ server/index.js
@@ -22,9 +22,7 @@ const isTest = process.env.NODE_ENV === "test" || !!process.env.VITE_TEST_BUILD;
 Shopify.Context.initialize({
   API_KEY: process.env.SHOPIFY_API_KEY,
   API_SECRET_KEY: process.env.SHOPIFY_API_SECRET,
-  //  SCOPES: process.env.SCOPES.split(","),
-  SCOPES: "",
-
+  SCOPES: process.env.SCOPES.split(","),
   HOST_NAME: process.env.HOST.replace(/https:\/\//, ""),
   API_VERSION: ApiVersion.April22,
   IS_EMBEDDED_APP: true,
